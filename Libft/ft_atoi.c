02bc3e793358662382c6cb57a2d659b106a75454
mravera <mravera@student.42lausanne.ch>
2021-12-01 13:35:52 +0100

    atoi ouioui

---
+++ Libft/ft_atoi.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+
+int     ft_atoi(const char *nptr)
+{
+        int                             i;
+        int                             sign;
+        unsigned int                    res;
+
+        sign = 1;
+        i = 0;
+        res = 0;
+        while ((9 <= nptr[i] && nptr[i] <= 13) || (nptr[i] == ' '))
+                i++;
+        while (nptr[i] == '-' || nptr[i] == '+')
+        {
+                if (nptr[i] == '-')
+                        sign = sign * -1;
+                i++;
+        }
+        while (ft_isdigit(nptr[i]))
+                res = (res * 10) + (nptr[i++] - 48);
+        return (sign * res);
+}
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+        (void)  argc;
+
+        printf("res = %d\n", ft_atoi(argv[1]));
+        return (0);
+}
