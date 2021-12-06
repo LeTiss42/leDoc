9c6de241ec6f80030dc54264dcfbd85e99763c73
Mathis Ravera <mravera@student.42lausanne.ch>
2021-12-06 22:05:16 +0100

    ft_split sans protec malloc + gitignore splittester

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mravera <@student.42lausanne.ch>	    +#+  +:+	   +#+	      */
 /*						  +#+#+#+#+#+	+#+	      */
 /*   Created: 2021/11/24 16:20:55 by mravera	       #+#    #+#	      */
-/*   Updated: 2021/12/03 21:51:15 by mathis           ###   ########.fr       */
+/*   Updated: 2021/12/06 19:58:30 by mravera          ###   ########.fr       */
 /*									      */
 /* ************************************************************************** */
 
@@ -41,5 +41,6 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
 
 #endif
